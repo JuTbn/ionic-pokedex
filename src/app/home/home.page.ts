590e943b825a944f9eb3bdf9f61d4f18178a99e6
JuTbn <jtoubon96@gmail.com>
2022-03-22 00:13:55 +0100

    show a short list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { PokemonService } from './../services/pokemon.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  offset = 0;
+  pokemon = [];
 
-  constructor() {}
+  constructor(private pokeService: PokemonService) {}
+
+  ngOnInit(){
+    this.loadPokemon();
+  }
+
+  loadPokemon(){
+    this.pokeService.getPokemon(this.offset)
+    .subscribe(res => {
+      console.log('result = ', res);
+      this.pokemon = res;
+      
+    })
+  }
 
 }
